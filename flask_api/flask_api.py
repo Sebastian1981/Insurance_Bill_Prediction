26742888a1715cd3ace4a4aec0d434d8dee094fd
Sebastian Domsch <Sebastian.Domsch@googlemail.com>
2022-09-15 17:03:32 +0200

    update flask api

--- flask_api/flask_api.py
+++ flask_api/flask_api.py
@@ -1,20 +1,27 @@
-from flask import Flask,request, jsonify
-from pycaret.regression import *
+from flask import Flask, request, jsonify
+import joblib
 import pandas as pd
-import numpy as np
 
 app = Flask(__name__)
 
-model = load_model('deployment_14092022')
+
+@app.route('/')
+def home():
+    return "<h1>Demo API: Predicting insurance bills!</h1>"  
+
+# import trained model
+model = joblib.load("./deployment_14092022.pkl")
+#model = load_model('deployment_14092022')
 cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
-    prediction = predict_model(model, data=data_unseen, round=0)
-    output = int(prediction.Label[0])
+    prediction = model.predict(data_unseen)
+    output = int(prediction)
     return jsonify(output)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
+app.run(host="0.0.0.0", port=int("5000"), debug=True)
