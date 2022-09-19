f9b493584efa4400e5cf25efa0da7cac1f43c3aa
Sebo <sebastian.domsch@gmail.com>
2022-09-19 10:18:37 +0200

    update images in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 from pycaret.regression import *
 from PIL import Image
 
-st.title('Demo App: Predicting Health Insurance Costs for Weini')
+st.title('Demo App: Predicting Health Insurance Costs')
 
 # Initialize session state vars
 if 'sex' not in st.session_state:
@@ -12,39 +12,28 @@ if 'sex' not in st.session_state:
 if 'smoker' not in st.session_state:
     st.session_state.smoker = 'no'
 
-# load weini image
-if st.session_state.sex == 'male' and st.session_state.smoker == 'no':
-    image = Image.open('./images/basti_original.png')
-    st.image(image, caption='This is handsome Basti!')
-elif st.session_state.sex == 'male' and st.session_state.smoker == 'yes':
-    image = Image.open('./images/basti_original_smoking.png')
-    st.image(image, caption='This is unhealthy Basti!')
-
-elif st.session_state.sex == 'female' and st.session_state.smoker == 'no':
-    image = Image.open('./images/basti_female.png')
-    st.image(image, caption='This is handsome Basti at its best!')
-elif st.session_state.sex == 'female' and st.session_state.smoker == 'yes':
-    image = Image.open('./images/basti_female_smoking.png')
-    st.image(image, caption='This is unhealthy Basti at its best!')
-
-elif st.session_state.sex == 'other' and st.session_state.smoker == 'no':
-    image = Image.open('./images/basti_other.png')
-    st.image(image, caption='Handsome Basti at its very best!')
-elif st.session_state.sex == 'other' and st.session_state.smoker == 'yes':
-    image = Image.open('./images/basti_other_smoking.png')
-    st.image(image, caption='Unhealthy Basti at its very best!')    
+# load images
+if st.session_state.sex == 'male':
+    image = Image.open('./images/male.png')
+    st.image(image, caption='You selected "male" gender.')
+elif st.session_state.sex == 'female':
+    image = Image.open('./images/female.png')
+    st.image(image, caption='You selected "female" gender.')
+elif st.session_state.sex == 'other':
+    image = Image.open('./images/no_gender.png')
+    st.image(image, caption='You selected "other" gender.')    
 
 # load trained model
 model = load_model('deployment_14092022')
 
 # input features
 features = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
-age = st.slider('select Basti´s age', 15,85,40)
-sex = st.selectbox('choose Bastis perceived gender', options = ['male', 'female','other'], key='sex')
-children = st.slider('select Basti´s children', 0,5,1)
-smoker = st.selectbox('choose if Basti a smoker', options = ['no', 'yes'], key='smoker')
-bmi = st.slider('choose Bastis perceived bmi', 15,55,30)
-region = st.selectbox('choose Basti´s desired region', options = ['southwest', 'southeast', 'northwest', 'northeast', 'other'])
+age = st.slider('select age', 25,65,40)
+sex = st.selectbox('select gender', options = ['male', 'female','other'], key='sex')
+children = st.slider('select number of children', 0,5,1)
+smoker = st.selectbox('select if smoker', options = ['no', 'yes'], key='smoker')
+bmi = st.slider('select bmi', 20,45,30)
+region = st.selectbox('select region', options = ['southwest', 'southeast', 'northwest', 'northeast', 'other'])
 input = np.array([age, sex, bmi, children, smoker, region])
 data_unseen = pd.DataFrame([input], columns = features)
 
@@ -52,7 +41,7 @@ data_unseen = pd.DataFrame([input], columns = features)
 if st.button('predict bill'):
     prediction = predict_model(model, data=data_unseen, round = 0)
     prediction = int(prediction.Label[0])
-    st.header('Basti´s health insurance bill is expected to be {} happy dollars.'.format(prediction))
+    st.header('The health insurance bill is expected to be {} dollars.'.format(prediction))
 
 
 
